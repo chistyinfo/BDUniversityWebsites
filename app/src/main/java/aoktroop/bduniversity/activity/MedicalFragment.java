77a846043bb1728e0d726a5d5462db492c6594c9
Suuny <sunny_mhs@hotmail.com>
2015-08-27 23:08:32 +0600

    I comp Admob in Pub & Engi & pri & Inter & Medi & All university.

--- app/src/main/java/aoktroop/bduniversity/activity/MedicalFragment.java
+++ app/src/main/java/aoktroop/bduniversity/activity/MedicalFragment.java
@@ -3,13 +3,19 @@ package aoktroop.bduniversity.activity;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 
 import oaktroop.bduniversity.R;
 
@@ -18,6 +24,7 @@ import oaktroop.bduniversity.R;
 public class MedicalFragment extends Fragment {
     private ListView listViewUni;
     private Context context;
+    private View view;
 
     String[] uniName ={
             "Dhaka Medical College",
@@ -71,7 +78,8 @@ public class MedicalFragment extends Fragment {
                 ,uniName,null);
         listViewUni=(ListView)rootView.findViewById(R.id.list_view_medical);
         listViewUni.setAdapter(adapter);
-
+        addVisibile(rootView);
+        view=rootView;
 
         listViewUni.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
@@ -94,10 +102,35 @@ public class MedicalFragment extends Fragment {
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
+        context=activity;
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        addVisibile(view);
+    }
+    private void addVisibile(View view) {
+
+        LinearLayout adLinearLayout=(LinearLayout)view.findViewById(R.id.adLaMedi);
+        if(isNetworkAvailable()) {
+            adLinearLayout.setVisibility(View.VISIBLE);
+            AdView mAdView = (AdView)view.findViewById(R.id.adViewMedi);
+            AdRequest adRequest = new AdRequest.Builder().build();
+            mAdView.loadAd(adRequest);
+        }
+        else {
+            adLinearLayout.setVisibility(View.GONE);
+        }
+    }
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 }
--- app/src/main/java/aoktroop/bduniversity/activity/NationalFragment.java
+++ app/src/main/java/aoktroop/bduniversity/activity/NationalFragment.java
@@ -3,13 +3,19 @@ package aoktroop.bduniversity.activity;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 
 import oaktroop.bduniversity.R;
 
@@ -18,6 +24,7 @@ import oaktroop.bduniversity.R;
 public class NationalFragment extends Fragment {
     private ListView listViewUni;
     private Context context;
+    private View view;
 
     String[] uniName ={
             "Bangladesh National University",
@@ -110,7 +117,8 @@ public class NationalFragment extends Fragment {
                 ,uniName,null);
         listViewUni=(ListView)rootView.findViewById(R.id.list_view_national);
         listViewUni.setAdapter(adapter);
-
+          addVisibile(rootView);
+          view=rootView;
 
         listViewUni.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
@@ -133,10 +141,35 @@ public class NationalFragment extends Fragment {
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
+        context=activity;
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        addVisibile(view);
+    }
+    private void addVisibile(View view) {
+
+        LinearLayout adLinearLayout=(LinearLayout)view.findViewById(R.id.adLaNat);
+        if(isNetworkAvailable()) {
+            adLinearLayout.setVisibility(View.VISIBLE);
+            AdView mAdView = (AdView)view.findViewById(R.id.adViewNat);
+            AdRequest adRequest = new AdRequest.Builder().build();
+            mAdView.loadAd(adRequest);
+        }
+        else {
+            adLinearLayout.setVisibility(View.GONE);
+        }
+    }
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 }
