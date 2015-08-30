dc858c29f0482984fe393d74dc0f2c189f4f3821
Hasan Abdullah <hasan_cse91@yahoo.com>
2015-08-31 00:05:37 +0600

    Update web icon.

---
+++ app/src/main/java/aoktroop/bduniversity/PostMethod.java
@@ -0,0 +1,84 @@
+package aoktroop.bduniversity;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Hasan Abdullah on 16-Jul-15.
+ */
+public class PostMethod extends AsyncTask<String,Void,String> {
+    private Context context;
+    public MyInterface myInterface = null;
+
+    public PostMethod(Context context){
+        this.context = context;
+    }
+
+    @Override
+    protected String doInBackground(String... arg0) {
+
+        String packageName = "oaktroop.bduniversity";
+        String link = "http://apitest.oaktroop.com/api/v1/test"; //API link
+
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpPost httpPost = new HttpPost(link);
+        HttpResponse response = null;
+        String line = "";
+
+        List<NameValuePair> nameValuePair = new ArrayList<NameValuePair>(2);
+
+        nameValuePair.add(new BasicNameValuePair("package_name", packageName));
+        nameValuePair.add(new BasicNameValuePair("name", arg0[0]));
+        nameValuePair.add(new BasicNameValuePair("email", arg0[1]));
+        nameValuePair.add(new BasicNameValuePair("phone", arg0[2]));
+        nameValuePair.add(new BasicNameValuePair("feedback", arg0[3]));
+
+        try {
+            httpPost.setEntity(new UrlEncodedFormEntity(nameValuePair));
+        }
+        catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            response = httpClient.execute(httpPost);
+
+            BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+
+            while ((line = rd.readLine()) != null) {
+                System.out.println("gha hghghgha aa: " + line);
+                break;
+            }
+
+        }
+        catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return line;
+    }
+
+    @Override
+    protected void onPostExecute(String result){
+        myInterface.processFinish(result);
+    }
+}
--- app/src/main/java/oaktroop/bduniversity/FeedbackActivity.java
+++ app/src/main/java/oaktroop/bduniversity/FeedbackActivity.java
@@ -7,28 +7,69 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 
-public class FeedbackActivity extends AppCompatActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import aoktroop.bduniversity.MyInterface;
+import aoktroop.bduniversity.PostMethod;
+
+public class FeedbackActivity extends AppCompatActivity implements MyInterface {
 
     Button button;
+    EditText name;
+    EditText email;
+    EditText phone;
+    EditText feedbackEditText;
+    String nameString;
+    String emailString;
+    String phoneString;
+    String feedbackString;
+    PostMethod postMethod = new PostMethod(this);
+    String strJson;
+    String data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
         addVisibile();
+        initialize();
+        postMethod.myInterface = this;
 
-        button = (Button) findViewById(R.id.submitButton);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+                nameString = name.getText().toString();
+                emailString = email.getText().toString();
+                phoneString = phone.getText().toString();
+                feedbackString = feedbackEditText.getText().toString();
+
+                if(!feedbackString.isEmpty()){
+                    postMethod.execute(nameString,emailString,phoneString,feedbackString);
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"You must give feedback!", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
+    }
+
+    private void initialize() {
+        button = (Button) findViewById(R.id.submitButton);
+        name = (EditText) findViewById(R.id.name);
+        email = (EditText) findViewById(R.id.email);
+        phone = (EditText) findViewById(R.id.phone);
+        feedbackEditText = (EditText) findViewById(R.id.feedback);
     }
 
     private void addVisibile() {
@@ -50,6 +91,23 @@ public class FeedbackActivity extends AppCompatActivity {
                 = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
         return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    @Override
+    public void processFinish(String output) {
+
+        String message = null;
+
+        try {
+            JSONObject reader = new JSONObject(output);
+            message = reader.getString("response");
+        }
+        catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+//        responseTextView.setText(message);
+
     }
 
 }
